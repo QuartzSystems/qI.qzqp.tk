20cb8498a5b7f8ab4e53e20a29d2f6de5291acf2
qsysmine <echomanari@gmail.com>
2015-06-01 04:02:15 +0000

    Fixed API link

--- a.js
+++ a.js
@@ -13,7 +13,7 @@
         fb.child(p).once("value", function(s) {
             var url = s.val();
             $('#loadedImage').attr("src",url);
-            $('#imageLink').attr("href", "http://qia.qzqp.tk/i/" + h.split("#")[1]);
+            $('#imageLink').attr("href", "http://a.qi.qzqp.tk/i/" + h.split("#")[1]);
         });
     };
     a();
